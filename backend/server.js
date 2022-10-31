530cdbba650f6bf9c8dd63f0efac76c870f68a7b
Mitchell Sappington <msappington11@gmail.com>
2022-10-31 15:50:31 -0500

    added some initial queries to the backend

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv')
 const path = require('path')
-const { Pool } = require('pg')
+const { Pool } = require('pg');
+const { response } = require('express');
 
 dotenv.config({path: '../.env'})
 const PORT = process.env.PORT || 1111 // this needs to match proxy in front-end package.json
@@ -25,3 +26,43 @@ pool.connect()
 
 // start listening
 app.listen(PORT, () => console.log('Server on PORT: ' + PORT))
+
+app.get('/persons', (req, response) => {
+    pool.query('SELECT * FROM PERSONS', (err, res) => {
+        if(err) {
+            response.json({err: err})
+            return
+        }
+        response.json({rows: res.rows})
+    })
+})
+
+app.get('/login', (req, response) => {
+    let email = req.query.email
+    let password = req.query.password
+    pool.query('SELECT IsAdmin FROM PERSONS WHERE UserEmail = $1 AND Password = $2', [email, password], (err, res) => {
+        if(err) {
+            response.json({err: err})
+            return
+        }
+        if(res.rows.length > 0) { // if user exists
+            response.json({exists: true, isAdmin: res.rows[0]}) // send isAdmin boolean
+            return
+        }
+        response.json({exists: false}) // user does not exist
+    })
+})
+
+app.get('/createAccount', (req, response) => {
+    let email = req.query.email
+    let password = req.query.password
+    let phone = req.query.phone
+    let name = req.query.name
+    pool.query('INSERT INTO PERSONS(UserName, UserEmail, UserPhone, UserPassword, IsAdmin) VALUES ($1, $2, $3, $4, false)', [name, email, phone, password], (req, res) => {
+        if(err) {
+            response.json({err: err})
+            return
+        }
+        response.json({err: false})
+    })
+})
