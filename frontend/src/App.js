e1fba4e4a31daf1ac9bdfb2af90df1ba730c1fb6
msappington11 <msappington11@gmail.com>
2022-11-17 16:25:40 -0600

    removed global.js and commented out test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,15 +8,6 @@ import UserPage from "./pages/UserPage";
 
 function App() {
 
-  // let options = {
-  //   method: 'GET',
-  //   url: `${url}/createAccount`,
-  //   params: {email: 'testEmail', password: 'testPassword', phone: '111-111-1111', name: 'testName'}
-  // }
-  // axios.request(options).then((res) => {
-  //   console.log(res.data)
-  // })
-
   return (
     <div className="App">
       <BrowserRouter>
