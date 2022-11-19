f0a400103eca28ec5ba441cc1f978fdb46f459b6
prometheus1400 <prometheus1400@gmail.com>
2022-11-18 20:37:12 -0600

    login sets userContext and logout works

--- frontend/src/components/LogPage/LoginPage.js
+++ frontend/src/components/LogPage/LoginPage.js
@@ -4,7 +4,8 @@ import Logo from "./styles/logo.png"
 import { Grid, TextField, Button, Stack } from "@mui/material"
 import { Link, useNavigate } from 'react-router-dom';
 import axios from "axios";
-
+import { useContext } from "react";
+import { UserContext } from "../../context/UserContext"
 
 function LoginPage() {
 
@@ -13,6 +14,8 @@ function LoginPage() {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [errorMessage, setErrorMessage] = React.useState("");
+
+    const {setUser} = useContext(UserContext)
 
     const handleUsernameChange = ({ target }) => {
         setUsername(target.value);
@@ -23,60 +26,65 @@ function LoginPage() {
     };
 
     const handleClick = () => {
-        var queryedUsername = "";
-        var queryedPassword = "";
-
-        const res = axios.get('/login', {
-            headers: {
-            'email': queryedUsername,
-            'password': queryedPassword
+        axios.get('/login', {
+            params: {
+                email: username,
+                password: password,
             }
-        });
+        })
+            .then((resp) => {
+                const { userID, isAdmin, exists } = resp.data
+                console.log("userID", userID)
+                console.log("isAdmin", isAdmin)
 
-        if(username === queryedUsername && password === queryedPassword){
-            navigate("/home");
-        }
-        else{
-            setErrorMessage("WRONG USERNAME OR PASSWORD!");
-        }
-
-        setUsername("")
-        setPassword("")        
+                if (exists) {
+                    setUsername("")
+                    setPassword("")
+                    setErrorMessage("")
+                    setUser({
+                        userID: userID,
+                        isAdmin: isAdmin,
+                    })
+                    navigate("/home");
+                }
+                else {
+                    setErrorMessage("WRONG USERNAME OR PASSWORD!");
+                }
+            })
     };
 
     return (
-      <div className="LoginPage">
-        <Grid
-            container
-            direction="row"
-            justifyContent="center"
-            alignItems="center"
-        >
-            <Grid item xs={6} style={{textAlign: "center"}}>
-                <img src={Logo} className="AppLogo" alt="Logo"></img>
+        <div className="LoginPage">
+            <Grid
+                container
+                direction="row"
+                justifyContent="center"
+                alignItems="center"
+            >
+                <Grid item xs={6} style={{ textAlign: "center" }}>
+                    <img src={Logo} className="AppLogo" alt="Logo"></img>
+                </Grid>
+                <Grid item xs={6}>
+                    <Stack
+                        direction="column"
+                        justifyContent="center"
+                        alignItems="center"
+                        spacing={1}
+                    >
+                        <TextField id="standard-basic" label="Username" variant="standard" alt="LoginUsername" onChange={handleUsernameChange} value={username}></TextField>
+                        <TextField id="standard-basic" label="Password" variant="standard" alt="LoginPassword" onChange={handlePasswordChange} value={password}></TextField>
+                        <Button variant="contained" onClick={handleClick}>Log In</Button>
+                        {errorMessage && <div className="error"> {errorMessage} </div>}
+                        <p>Don't have an account?
+                            <Link to="/signin">
+                                <Button variant="text">Sign in</Button>
+                            </Link>
+                        </p>
+                    </Stack>
+                </Grid>
             </Grid>
-            <Grid item xs={6}>
-                <Stack
-                    direction="column"
-                    justifyContent="center"
-                    alignItems="center"
-                    spacing={1}
-                >
-                    <TextField id="standard-basic" label="Username" variant="standard" alt="LoginUsername" onChange={handleUsernameChange} value={username}></TextField>
-                    <TextField id="standard-basic" label="Password" variant="standard" alt="LoginPassword" onChange={handlePasswordChange} value={password}></TextField>
-                    <Button variant="contained" onClick={handleClick}>Log In</Button>
-                    {errorMessage && <div className="error"> {errorMessage} </div>}
-                    <p>Don't have an account? 
-                        <Link to="/signin">
-                            <Button variant="text">Sign in</Button> 
-                        </Link>
-                    </p>
-                </Stack>
-            </Grid>
-        </Grid>
-      </div>
+        </div>
     );
 }
-  
+
 export default LoginPage;
-  
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -4,9 +4,12 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
 import { useNavigate } from "react-router-dom";
+import { useContext } from 'react';
+import { UserContext } from '../context/UserContext';
 
 function Navbar() {
     const navigate = useNavigate();
+    const { setUser } = useContext(UserContext)
 
     return (
         <Box sx={{ flexGrow: 1 }}>
@@ -14,31 +17,36 @@ function Navbar() {
                 <Toolbar>
                     {/* change to icon button use therapy logo */}
                     <Button
-                        onClick={() => {navigate("/home", { replace: true})}}
+                        onClick={() => { navigate("/home", { replace: true }) }}
                         sx={{ my: 2, color: 'white', display: 'block' }}
                     >
                         Home
                     </Button>
                     <Button
-                        onClick={() => {navigate("/appointments", { replace: true})}}
+                        onClick={() => { navigate("/appointments", { replace: true }) }}
                         sx={{ my: 2, color: 'white', display: 'block' }}
                     >
                         Appointments
                     </Button>
                     <Button
-                        onClick={() => {navigate("/reviews", { replace: true})}}
+                        onClick={() => { navigate("/reviews", { replace: true }) }}
                         sx={{ my: 2, color: 'white', display: 'block' }}
                     >
                         Reviews
                     </Button>
                     <Button
-                        onClick={() => {navigate("/users", { replace: true})}}
+                        onClick={() => { navigate("/users", { replace: true }) }}
                         sx={{ my: 2, color: 'white', display: 'block' }}
                     >
                         Users
                     </Button>
 
-                    <Button color="inherit" sx={{ marginLeft: 'auto' }}>Logout</Button>
+                    <Button color="inherit" sx={{ marginLeft: 'auto' }} onClick={() => {
+                        setUser(null)
+                        navigate("/", { replace: true })
+                    }}>
+                        Logout
+                    </Button>
                 </Toolbar>
             </AppBar>
         </Box>
