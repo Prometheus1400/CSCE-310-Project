b4bc8791dd6190dce05c81653bfe9828beaa2db9
prometheus1400 <prometheus1400@gmail.com>
2022-12-05 18:14:18 -0600

    fixed text fields, changed logo color

--- frontend/src/components/LogPage/SigninPage.js
+++ frontend/src/components/LogPage/SigninPage.js
@@ -1,9 +1,28 @@
 import React, { useState } from "react";
 import "./styles/LoginPage.css"
-import Logo from "./styles/logo.png"
+import Logo from "./styles/logo-white.png"
 import { Grid, TextField, Button, Stack, Checkbox, FormGroup, FormControlLabel } from "@mui/material"
+import { styled } from "@mui/material/styles";
 import { Link } from 'react-router-dom';
 import axios from "axios";
+
+const StyledTextField = styled(TextField)({
+    '& .MuiInput-underline:before': {
+        borderBottomColor: 'rgb(171, 174, 178)',
+    },
+    '&:hover .MuiInput-underline:before': {
+        borderBottomColor: 'white',
+    },
+    '& label': {
+        color: 'rgb(171, 174, 178)',
+    },
+    '&:hover label': {
+        color: 'white',
+    },
+    '& input': {
+        color: 'rgb(171, 174, 178)',
+    },
+});
 
 function SigninPage() {
 
@@ -43,7 +62,7 @@ function SigninPage() {
     };
 
     return (
-      <div className="SigninPage">
+      <div className="SigninPage" style={{color:"rgb(171, 174, 178)"}}>
         <Grid
             container
             direction="row"
@@ -60,13 +79,13 @@ function SigninPage() {
                     alignItems="center"
                     spacing={1}
                 >
-                    <TextField id="standard-basic" label="Firstname" variant="standard" alt="SigninFirstName" onChange={handleFirstnameChange} value={firstname}></TextField>
-                    <TextField id="standard-basic" label="Lastname" variant="standard" alt="SigninLastName" onChange={handleLastnameChange} value={lastname}></TextField>
-                    <TextField id="standard-basic" label="Email" variant="standard" alt="SigninEmail" onChange={handleEmailChange} value={email}></TextField>
-                    <TextField id="standard-basic" label="Password" variant="standard" alt="SigninPassword" onChange={handlePasswordChange} value={password}></TextField>
+                    <StyledTextField id="standard-basic" label="Firstname" variant="standard" alt="SigninFirstName" onChange={handleFirstnameChange} value={firstname}></StyledTextField>
+                    <StyledTextField id="standard-basic" label="Lastname" variant="standard" alt="SigninLastName" onChange={handleLastnameChange} value={lastname}></StyledTextField>
+                    <StyledTextField id="standard-basic" label="Email" variant="standard" alt="SigninEmail" onChange={handleEmailChange} value={email}></StyledTextField>
+                    <StyledTextField id="standard-basic" label="Password" variant="standard" alt="SigninPassword" onChange={handlePasswordChange} value={password}></StyledTextField>
                     <FormGroup>
-                        <FormControlLabel control={<Checkbox  />} label="Admin" />
-                        <FormControlLabel control={<Checkbox />} label="User" />
+                        <FormControlLabel control={<Checkbox  sx={{color:"rgb(171, 174, 178)"}}/>} label="Admin" />
+                        <FormControlLabel control={<Checkbox sx={{color:"rgb(171, 174, 178)"}}/>} label="User" />
                     </FormGroup>
                     <Button variant="contained" onClick={handleClick}>Sign Up</Button>
                     <p>Have an account? 
